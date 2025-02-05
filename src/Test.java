6baab6d8f7afb27bbf3c2ce2cf65919226c0a764
lyx <3237250978@qq.com>
2025-02-05 23:51:16 +0800

    修改了Test.java文件

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("ni hao");
+        System.out.println("hahahahahahha");
     }
 
 }
