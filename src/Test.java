e4a87cd50c95988b23d1239dad427e710b5d38f1
lyx <3237250978@qq.com>
2025-02-05 23:54:26 +0800

    第二次修改

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("ni hao");
-        System.out.println("hahahahahahha");
+        System.out.println("I love you");
+        System.out.println("I love you too");
     }
 
 }
